5120a2ca6ca29a971c73a6e3c3cb4c3eecbc5ed8
superCreature0723 <mosesberggren2@gmail.com>
2025-04-29 04:05:16 +0300

    add GameView component

---
+++ src/app/components/Scores.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import ScoreBox from "./ScoreBox"
+
+const Scores = () => {
+    return (
+        <div className="w-full flex py-6 px-8 justify-center items-center gap-12">
+            <ScoreBox title="Win" score={0} />
+            <ScoreBox title="Equal" score={0} />
+            <ScoreBox title="Win" score={0} />
+        </div>
+    )
+}
+
+export default Scores
--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,7 +1,14 @@
+import GameView from "../components/GameView"
+import Round from "../components/Round"
+import Scores from "../components/Scores"
 
 const GamePage = () => {
     return (
-        <div className="w-full min-h-screen bg-stone-200">GamePage</div>
+        <div className="w-full min-h-screen bg-stone-200 flex flex-col">
+            <Scores />
+            <Round />
+            <GameView />
+        </div>
     )
 }
 
