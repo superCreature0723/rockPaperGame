db665e5375dc020b67990fbf69a86cfcf18f69d2
superCreature0723 <mosesberggren2@gmail.com>
2025-04-29 03:59:15 +0300

    add the gameRoom

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
-import Footer from "./components/Footer";
-import NavBar from "./components/NavBar";
-import Time from "./components/Time";
+import Image from "next/image"
+import Link from "next/link"
 
 export default function Home() {
   return (
-    <main className="w-full min-h-screen bg-indigo-400">
-      <NavBar />
-      <Time />
-      <Footer />
+    <main className="w-full flex flex-col gap-8 items-center justify-center min-h-screen bg-stone-200">
+      <Image src={"/images/banner.png"} width={300} height={300} alt={"banner"} />
+      <Link href={"/game"} className="text-2xl font-light uppercase underline underline-offset-8 text-yellow-900">
+        click me to start
+      </Link>
     </main>
   )
 }
