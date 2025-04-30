49067d9501046eb8973a70be2b6d970d500235a2
superCreature0723 <mosesberggren2@gmail.com>
2025-04-30 04:35:52 +0300

    fix the responsive of the GameView

--- src/app/components/ScoreBox.tsx
+++ src/app/components/ScoreBox.tsx
@@ -13,24 +13,29 @@ function ScoreBox({ score, title, scoreColor }: ScoreBoxPropsType) {
   const [change, setChange] = useState(true);
 
   useEffect(() => {
-    setChange(!change);
+    if (+score > 0) {
+      setChange(!change);
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [score]);
 
   const animation1 = change ? "fold" : "unfold";
   const animation2 = !change ? "fold" : "unfold";
-  const number1 = change ? +score - 1 : score;
-  const number2 = !change ? +score - 1 : score;
+  const oldNumber = +score > 0 ? +score - 1 : score;
+  const number1 = change ? oldNumber : score;
+  const number2 = !change ? oldNumber : score;
 
   return (
-    <div className="flex flex-col justify-center items-center gap-3">
-      <span className="text-xl bg-zinc-700 text-white py-1 px-3">{title}</span>
+    <div className="flex flex-col justify-center items-center">
+      <span className="w-[90px] text-xl text-center bg-zinc-700 text-white py-1 px-3">
+        {title}
+      </span>
       <div className="flipCounter">
         <div className="upperCard">
           <span className={`${scoreColor}`}>{score}</span>
         </div>
         <div className="lowerCard">
-          <span className={`${scoreColor}`}>{+score - 1}</span>
+          <span className={`${scoreColor}`}>{oldNumber}</span>
         </div>
         <div className={`flipCard first ${animation1}`}>
           <span className={`${scoreColor}`}>{number1}</span>
